1999fc7b52551cb2f228cbfb3ba0ded07259e6e4
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-10-25 09:11:21 +0530

    firstProgram.java

---
+++ Basics/firstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi Harsh");
+        System.out.println();
+        System.out.print("Hi Soumyadeb\n");
+        System.out.println("Hi Yuvi");
+        System.out.println("Hi Sarthak");
+        System.out.print("Hi Kavisha");
+    }
+}
